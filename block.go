e72e4930462e30ca65973d2e2ba86602d8a5c0d8
Jeromy <jeromyj@gmail.com>
2018-03-06 01:31:34 -0800

    add a block sync method

--- block.go
+++ block.go
@@ -1,43 +1,49 @@
 package main
 
 import (
-    "encoding/json"
-    "crypto/sha256"
-    "encoding/base64"
+	"crypto/sha256"
+	"encoding/json"
+
+	cbor "gx/ipfs/QmRVSCwQtW1rjHCay9NqKXDwbtKTgDcN4iY7PrpSqfKM5D/go-ipld-cbor"
+	mh "gx/ipfs/QmZyZDi491cCNTLfAhwcaDii2Kg4pwKRkhqQzURGDvY6ua/go-multihash"
+	"gx/ipfs/QmcZfnkapfECQGcLZaf9B79NRg7cRa9EnZh4LSbkCzwNvY/go-cid"
 )
 
 type Block struct {
-    PrevHash [32]byte
-    Transactions []Transaction
-    Height uint64
-    Time uint64
-    Nonce []byte
-    Solution string
+	PrevHash     *cid.Cid
+	Transactions []Transaction
+	Height       uint64
+	Time         uint64
+	Nonce        []byte
+	Solution     string
 }
 
-func (b *Block) Serialize() ([]byte){
-    data, err := json.Marshal(b)
-    if err != nil {
-        panic(err)
-    }
-    return data
+func (b *Block) Serialize() []byte {
+	data, err := json.Marshal(b)
+	if err != nil {
+		panic(err)
+	}
+	return data
 }
 
-func DeserializeBlock(buf []byte) (*Block, error){
-    var blk Block
-    err := json.Unmarshal(buf, &blk)
-    if err != nil {
-        return nil, err
-    }
-    return &blk, nil
+func DeserializeBlock(buf []byte) (*Block, error) {
+	var blk Block
+	err := json.Unmarshal(buf, &blk)
+	if err != nil {
+		return nil, err
+	}
+	return &blk, nil
 }
 
-func (b *Block) GetHash() ([32]byte){
-    ser := b.Serialize() 
-    return sha256.Sum256(ser)
+func (b *Block) GetCid() *cid.Cid {
+	nd, err := cbor.WrapObject(b, mh.SHA2_256, -1)
+	if err != nil {
+		panic(err)
+	}
+
+	return nd.Cid()
 }
 
-func (b *Block) GetHashString() string {
-    hash := b.GetHash()
-    return base64.StdEncoding.EncodeToString(hash[:])
+func (b *Block) GetHash() [32]byte {
+	return sha256.Sum256(b.Serialize())
 }
--- node.go
+++ node.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "time"
-    "os"
-    
-    libp2p "gx/ipfs/QmNh1kGFFdsPu79KNSaL4NUKUPb4Eiz4KHdMtFY6664RDp/go-libp2p"
-    host "gx/ipfs/QmNmJZL7FQySMtE2BQuLMuZg2EB2CLEunJJUSVSc9YnnbV/go-libp2p-host"
-    ipfsaddr "gx/ipfs/QmQViVWBHbU6HmYjXcdNq7tVASCNgdg64ZGcauuDkLCivW/go-ipfs-addr"
-    floodsub "gx/ipfs/QmSFihvoND3eDaAYRCeLgLPt62yCPgMZs1NSZmKFEtJQQw/go-libp2p-floodsub"
-    peerstore "gx/ipfs/QmXauCuJzmzapetmC6W4TuDJLL1yFFrVzSHoWv8YdbmnxH/go-libp2p-peerstore"
-    
-    "./types"
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	libp2p "gx/ipfs/QmNh1kGFFdsPu79KNSaL4NUKUPb4Eiz4KHdMtFY6664RDp/go-libp2p"
+	host "gx/ipfs/QmNmJZL7FQySMtE2BQuLMuZg2EB2CLEunJJUSVSc9YnnbV/go-libp2p-host"
+	ipfsaddr "gx/ipfs/QmQViVWBHbU6HmYjXcdNq7tVASCNgdg64ZGcauuDkLCivW/go-ipfs-addr"
+	floodsub "gx/ipfs/QmSFihvoND3eDaAYRCeLgLPt62yCPgMZs1NSZmKFEtJQQw/go-libp2p-floodsub"
+	peerstore "gx/ipfs/QmXauCuJzmzapetmC6W4TuDJLL1yFFrVzSHoWv8YdbmnxH/go-libp2p-peerstore"
+
+	"./types"
 )
 
 type Node struct {
-	p2pNode host.Host
-	mempool *Mempool
+	p2pNode    host.Host
+	mempool    *Mempool
 	blockchain *Blockchain
-    pubsub *floodsub.PubSub
+	pubsub     *floodsub.PubSub
 }
 
-func CreateNewNode(ctx context.Context) (*Node){
-    var node Node
+func CreateNewNode(ctx context.Context) *Node {
+	var node Node
 
 	newNode, err := libp2p.New(ctx, libp2p.Defaults)
 	if err != nil {
@@ -38,109 +38,109 @@ func CreateNewNode(ctx context.Context) (*Node){
 	for i, addr := range newNode.Addrs() {
 		fmt.Printf("%d: %s/ipfs/%s\n", i, addr, newNode.ID().Pretty())
 	}
-    
-    if len(os.Args) > 1 {
-        addrstr := os.Args[1]
-        addr, err := ipfsaddr.ParseString(addrstr)
-        if err != nil {
-            panic(err)
-        }
-        fmt.Println("Parse Address:", addr)
 
-        pinfo, _ := peerstore.InfoFromP2pAddr(addr.Multiaddr())
+	if len(os.Args) > 1 {
+		addrstr := os.Args[1]
+		addr, err := ipfsaddr.ParseString(addrstr)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Parse Address:", addr)
 
-        if err := newNode.Connect(ctx, *pinfo); err != nil {
-            fmt.Println("bootstrapping a peer failed", err)
-        }
-    }
-        
-    blockchain := NewBlockchain(newNode)
-    
-    node.p2pNode = newNode
-    node.mempool = NewMempool()
-    node.pubsub = pubsub 
-    node.blockchain = blockchain
-    
-    node.ListenBlocks(ctx)
-    node.ListenTransactions(ctx)
-    
-    return &node
-    
+		pinfo, _ := peerstore.InfoFromP2pAddr(addr.Multiaddr())
+
+		if err := newNode.Connect(ctx, *pinfo); err != nil {
+			fmt.Println("bootstrapping a peer failed", err)
+		}
+	}
+
+	blockchain := NewBlockchain(newNode)
+
+	node.p2pNode = newNode
+	node.mempool = NewMempool()
+	node.pubsub = pubsub
+	node.blockchain = blockchain
+
+	node.ListenBlocks(ctx)
+	node.ListenTransactions(ctx)
+
+	return &node
+
 }
 
-func (node *Node) ListenBlocks(ctx context.Context){
-    sub, err := node.pubsub.Subscribe("blocks")
-    if err != nil {
-        panic(err)
-    }
-    go func(){
-        for {
-            msg, err := sub.Next(ctx)
-            if err != nil {
-                panic(err)
-            }
-            blk, err := DeserializeBlock(msg.GetData())
-            if err != nil {
-                panic(err)
-            }
-            // fmt.Println("Block received over network:", string(blk.Serialize()))
-            fmt.Println("Block received over network, blockhash", blk.GetHashString())
-            cid := node.blockchain.AddBlock(blk)
-            if cid != nil {
-                fmt.Println("Block added, cid:", cid)
-                node.mempool.removeTxs(blk.Transactions)
-            }
-        }
-    }()
+func (node *Node) ListenBlocks(ctx context.Context) {
+	sub, err := node.pubsub.Subscribe("blocks")
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			msg, err := sub.Next(ctx)
+			if err != nil {
+				panic(err)
+			}
+			blk, err := DeserializeBlock(msg.GetData())
+			if err != nil {
+				panic(err)
+			}
+			// fmt.Println("Block received over network:", string(blk.Serialize()))
+			fmt.Println("Block received over network, blockhash", blk.GetCid())
+			cid := node.blockchain.AddBlock(blk)
+			if cid != nil {
+				fmt.Println("Block added, cid:", cid)
+				node.mempool.removeTxs(blk.Transactions)
+			}
+		}
+	}()
 }
 
-func (node *Node) ListenTransactions(ctx context.Context){
-    sub, err := node.pubsub.Subscribe("transactions")
-    if err != nil {
-        panic(err)
-    }
-    go func(){
-        for {
-            msg, err := sub.Next(ctx)
-            if err != nil {
-                panic(err)
-            }
-            tx, err := DeserializeTx(msg.GetData())
-            if err != nil {
-                panic(err)
-            }
-            node.mempool.AddTx(tx)
-            fmt.Println("Tx received over network, added to mempool:", tx)
-        }
-    }()
+func (node *Node) ListenTransactions(ctx context.Context) {
+	sub, err := node.pubsub.Subscribe("transactions")
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			msg, err := sub.Next(ctx)
+			if err != nil {
+				panic(err)
+			}
+			tx, err := DeserializeTx(msg.GetData())
+			if err != nil {
+				panic(err)
+			}
+			node.mempool.AddTx(tx)
+			fmt.Println("Tx received over network, added to mempool:", tx)
+		}
+	}()
 }
 
 func (node *Node) CreateNewBlock() *Block {
-    var blk Block
-    blk.PrevHash = node.blockchain.Head.GetHash()
-    blk.Transactions = node.mempool.SelectTransactions()
-    blk.Height = node.blockchain.Head.Height + 1
-    blk.Time = uint64(time.Now().Unix())
-    return &blk
+	var blk Block
+	blk.PrevHash = node.blockchain.Head.GetCid()
+	blk.Transactions = node.mempool.SelectTransactions()
+	blk.Height = node.blockchain.Head.Height + 1
+	blk.Time = uint64(time.Now().Unix())
+	return &blk
 }
 
 func (node *Node) BroadcastBlock(block *Block) {
-    data := block.Serialize()
-    node.pubsub.Publish("blocks", data)
+	data := block.Serialize()
+	node.pubsub.Publish("blocks", data)
 }
 
 func (node *Node) SendTransaction(tx *Transaction) *types.SendTxResponse {
-    var res types.SendTxResponse
-    txid := tx.GetTxid()
-    node.mempool.transactions[txid] = *tx
-    data := tx.Serialize()
-    node.pubsub.Publish("transactions", data)
-    res.Txid = tx.GetTxidString()
-    return &res
+	var res types.SendTxResponse
+	txid := tx.GetTxid()
+	node.mempool.transactions[txid] = *tx
+	data := tx.Serialize()
+	node.pubsub.Publish("transactions", data)
+	res.Txid = tx.GetTxidString()
+	return &res
 }
 
 func (node *Node) GetInfo() *types.GetInfoResponse {
-    var res types.GetInfoResponse
-    res.BlockHeight = node.blockchain.Head.Height
-    return &res
+	var res types.GetInfoResponse
+	res.BlockHeight = node.blockchain.Head.Height
+	return &res
 }
